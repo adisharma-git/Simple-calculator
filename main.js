9bc51e58c74f681401eeb6647ceb565c93b1fb27
adisharma-git <143196543+adisharma-git@users.noreply.github.com>
2024-02-28 22:27:05 +0530

    Added to js file
    added the cleardisplay and calculate function to the js file

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ const display = document.getElementById("display");
 function appendtodisplay(input) {
   display.value += input;
 }
+function cleardisplay() {
+  display.value = "";
+}
+function calculate() {
+  display.value = eval(display.value);
+}
